a3d7399c342cf912956b6894f82201c47cebeca4
Viktoryia-Kasabutskaya <viktoryia.kasabutskaya@gmail.com>
2023-08-16 11:23:23 +0200

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = [];
+
+  const brackets = new Map(bracketsConfig);
+
+  for (const bracket of str) {
+    if (bracket === brackets.get(arr[arr.length - 1])) {
+      arr.pop();
+    } else {
+      arr.push(bracket);
+    }
+  }
+
+  return arr.length === 0;
+};
